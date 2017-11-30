15af1197c9963ada873e796455cb890cc86697c4
张晨 <windyzj@163.com>
2017-11-30 22:51:24 +0800

    123
    123123

--- src/com/atguigu/git/Dugu9jian.java
+++ src/com/atguigu/git/Dugu9jian.java
@@ -4,7 +4,7 @@ public class Dugu9jian {
 
 	public static void main(String[] args) {
  
-		 System.out.println("Hello 10jian");
+		 System.out.println("Hello 10jian22222");
 		 System.out.println("Hello 10jian");
  
 		 System.out.println("Hello 9jian");
